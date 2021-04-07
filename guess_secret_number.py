21db998282076fe8eb73c21a079f325429ff42ec
Ivo Gsellmann <ivo.gsellmann@gmx.at>
2021-04-07 18:33:36 +0200

    initial commit

---
+++ guess_secret_number.py
@@ -0,0 +1,8 @@
+secret = 54
+
+guess = int(input("Choose your number: "))
+
+if secret == guess:
+    print("Congratulation, you got the right number!")
+else:
+    print("Your guess is wrong! Try again!")
